ccbfdcb9d430adcdaece4e42e89a99ebae0644a3
antho1404 <anthony.estebe@gmail.com>
2025-01-07 18:24:29 +0700

    use public api

--- src/lib/liteflow.ts
+++ src/lib/liteflow.ts
@@ -4,7 +4,5 @@ const liteflowKey = process.env.NEXT_PUBLIC_LITEFLOW_API_KEY;
 if (!liteflowKey)
   throw new Error("NEXT_PUBLIC_LITEFLOW_API_KEY is not defined");
 
-const liteflow = new Liteflow(liteflowKey, {
-  baseUrl: "http://localhost:3000/api/v1",
-});
+const liteflow = new Liteflow(liteflowKey);
 export default liteflow;
