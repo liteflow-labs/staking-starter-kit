dcc45d574be6886ee682cf40756d6fc2bf27eda0
antho1404 <anthony.estebe@gmail.com>
2025-03-19 11:36:11 +0700

    update footer

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,8 @@ import { Providers } from "@/components/providers";
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import { headers } from "next/headers";
+import Image from "next/image";
+import Link from "next/link";
 import "./globals.css";
 
 const geistSans = Geist({
@@ -31,7 +33,22 @@ export default async function RootLayout({
       >
         <div className="absolute inset-0 bg-gradient-to-tr from-primary/20 to-accent/20 blur-xl -z-10" />
         <Providers cookie={(await headers()).get("cookie") || ""}>
-          {children}
+          <div className="flex flex-col min-h-screen">
+            <main className="flex-grow flex items-center justify-center px-4 sm:px-6 lg:px-8">
+              {children}
+            </main>
+            <footer className="text-center text-sm text-muted-foreground py-8 flex justify-center items-center gap-1">
+              Powered by{" "}
+              <Link href="https://liteflow.com">
+                <Image
+                  src="/liteflow.svg"
+                  alt="Liteflow Logo"
+                  width={96}
+                  height={24}
+                />
+              </Link>
+            </footer>
+          </div>
         </Providers>
       </body>
     </html>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,15 +9,8 @@ export default function Home() {
   if (!address) throw new Error("Missing staking contract address");
 
   return (
-    <div className="flex flex-col min-h-screen">
-      <main className="flex-grow flex items-center justify-center px-4 sm:px-6 lg:px-8">
-        <div className="w-full max-w-4xl mx-auto text-center flex flex-col items-center">
-          <Staking chainId={chainId} address={address} />
-        </div>
-      </main>
-      <footer className="py-6 text-center text-muted-foreground">
-        <p>&copy; {new Date().getFullYear()} Liteflow. All rights reserved.</p>
-      </footer>
+    <div className="w-full max-w-4xl mx-auto text-center flex flex-col items-center">
+      <Staking chainId={chainId} address={address} />
     </div>
   );
 }
