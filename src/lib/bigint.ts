bc8fd35b5e0dde8ad6945b0bf78a861ca574fd49
Nicolas Mahe <nicolas@mahe.me>
2025-03-21 16:45:33 +0700

    update strToBigInt to use parseUnits and catch invalid number error

--- src/lib/bigint.ts
+++ src/lib/bigint.ts
@@ -1,4 +1,10 @@
+import { InvalidDecimalNumberError, parseUnits } from "viem";
+
 export function strToBigInt(value: string, decimals: number = 18): bigint {
-  if (!value) return BigInt(0);
-  return BigInt(Math.floor(parseFloat(value) * 10 ** decimals));
+  try {
+    return parseUnits(value, decimals);
+  } catch (error) {
+    if (error instanceof InvalidDecimalNumberError) return BigInt(0);
+    throw error;
+  }
 }
