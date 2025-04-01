b26efe432409bfa32931d3987a1a2ab5601fde28
antho1404 <anthony.estebe@gmail.com>
2025-04-01 17:47:55 +0700

    add image transform

--- next.config.ts
+++ next.config.ts
@@ -5,6 +5,14 @@ const nextConfig: NextConfig = {
   experimental: {
     typedEnv: true,
   },
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "*",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
