62bc1034da2d0d76bcd83387855191ec9f3dd132
antho1404 <anthony.estebe@gmail.com>
2025-04-01 17:46:14 +0700

    add remote simulation

---
+++ src/hooks/useSimulation.ts
@@ -0,0 +1,35 @@
+import liteflow from "@/lib/liteflow";
+import { useQuery } from "@tanstack/react-query";
+
+export default function useSimulation({
+  chainId,
+  address,
+  opts,
+}: {
+  chainId: number;
+  address: string;
+  opts: {
+    tokenStaked: bigint;
+    nftStaked: number;
+  };
+}) {
+  return useQuery({
+    queryFn: async () => {
+      const res = await liteflow.stakingSimulation.retrieve(chainId, address, {
+        tokenStaked: opts.tokenStaked.toString(),
+        nftStaked: opts.nftStaked,
+      });
+      if (res.error) throw new Error(res.error.message);
+      return res.data;
+    },
+    queryKey: [
+      "simulation",
+      {
+        chainId,
+        address,
+        tokenStaked: opts.tokenStaked.toString(),
+        nftStaked: opts.nftStaked,
+      },
+    ],
+  });
+}
