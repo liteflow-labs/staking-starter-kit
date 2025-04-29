1c3384ccc99dfd272cd69121920dee3dd222047c
antho1404 <anthony.estebe@gmail.com>
2025-04-29 12:16:12 +0700

    default to public api

--- src/lib/liteflow.ts
+++ src/lib/liteflow.ts
@@ -4,8 +4,5 @@ const liteflowKey = process.env.NEXT_PUBLIC_LITEFLOW_API_KEY;
 if (!liteflowKey)
   throw new Error("NEXT_PUBLIC_LITEFLOW_API_KEY is not defined");
 
-const liteflow = new Liteflow(liteflowKey, {
-  baseUrl:
-    "https://liteflowcom-git-feature-multi-asset-staking-liteflow.vercel.app/api/v1",
-});
+const liteflow = new Liteflow(liteflowKey);
 export default liteflow;
